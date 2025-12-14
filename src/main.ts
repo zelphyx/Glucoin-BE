07d024aa22173f3a00e1180cf57d6aec38b8561d
zelphyx <zelphyx@Ilhamis-MacBook-Pro.local>
2025-12-14 09:39:24 +0700

    feat [IR] reminder, dashboard doctor, dll

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://glucoin.vercel.app',
+      'https://glucoin.mentorit.my.id',
+      process.env.FRONTEND_URL,
+    ].filter(Boolean) as string[],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+  });
 
 
 
