9d28038c5ef6230568892eeb3a435d84398a9c81
zelphyx <zelphyx@Ilhamis-MacBook-Pro.local>
2025-12-14 09:42:40 +0700

    feat [IR] reminder, dashboard doctor, dll

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
       'http://localhost:3001',
       'https://glucoin.vercel.app',
       'https://glucoin.mentorit.my.id',
+      'https://glucoinapi.mentorit.my.id',
       process.env.FRONTEND_URL,
     ].filter(Boolean) as string[],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
